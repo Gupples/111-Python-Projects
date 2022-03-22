6928250ee61d63166e62e3bde8271a1608f34231
Gupples <gupples224@gmail.com>
2022-03-21 22:33:01 -0600

    Practice for when Classes come up in Python.

---
+++ Classes_testing.py
@@ -0,0 +1,99 @@
+"""
+This is a document to get some practice in for making and using classes. I will
+have 3 teachers, each with a name, a subject they teach, and a classroom they
+teach in.
+
+Notes to self:
+NOTATION FOR OTHER PROGRAMMERS!!!
+"_" prefix = Only use/modify this property if you know what you're doing.
+"__" prefix = DO NOT USE!!!
+When writing, it's okay. If you're modifying someone else's work, FOLLOW.
+"""
+
+class Teacher():
+    # Constructor.
+    def __init__(self, name, subject, room):
+        self.name = name
+        self.subject = subject
+        self.room = room
+
+    # Define Getters
+    # Getter for name.
+    @property
+    def name(self):
+        return self.__name
+
+    # Getter for subject.
+    @property
+    def subject(self):
+        return self.__subject
+
+    # Getter for room.
+    @property
+    def room(self):
+        return self.__room
+
+
+    # Define Setters
+    # Setter for name.
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    # Setter for subject
+    @subject.setter
+    def subject(self, value):
+        self.__subject = value
+
+    # Setter for room 
+    @room.setter
+    def room(self, value):
+        self.__room = value
+
+def meet_teachers(list: list):
+    """
+    This will read off a list of teachers and tell you who they are, what they
+    teach, and where they teach it. 
+
+    Parameters:
+        Teachers (list): The list of the teachers
+    Returns:
+        nothing.
+    """
+    for item in list:
+        print(f"{item.name} teaches {item.subject} in room {item.room}.")
+    print()
+
+
+def main():
+    # Declare 3 teachers.
+    teacher1 = Teacher("Mr. Einstein", "Science", 116)
+    teacher2 = Teacher("Mr. Miyagi", "Cooking", 225)
+    teacher3 = Teacher("Mr. Snape", "Potions", 731)
+
+    # Make our list of teachers
+    teachers = [teacher1, teacher2, teacher3]
+
+    # Read list of teachers.
+    meet_teachers(teachers)
+
+    print("Hold on; that's not right. Here's the actual list of teachers and",
+    "what they teach.\n")
+
+    # Fix list of teachers
+    teacher1.name = "Ms. Frizzle"
+    teacher1.room = 242
+
+    teacher2.subject = "P.E."
+    teacher2.room = 105
+    
+    teacher3.name = "Prof. McGonnagal"
+    teacher3.subject = "Transfiguration"
+
+
+
+    # Read correct list of teachers.
+    meet_teachers(teachers)
+    
+if __name__ == "__main__":
+    main()
